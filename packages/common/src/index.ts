9a69c644bcf4a3178d86c10447509f4104403ff7
lakhan <lakhanlal3011@gmail.com>
2025-10-15 15:36:37 +0530

    added zod in packages

---
+++ packages/common/src/index.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod'
+
+export const signUpSchema = z.object({
+    username: z.email(),
+    password: z.string().min(6).max(20),
+    name: z.string().min(6).max(20)
+})
+
+export const signInSchema = z.object({
+    username: z.email(),
+    password: z.string()
+})
+
+export const createRoomSchema = z.object({
+    name: z.string().min(3).max(10),
+})
