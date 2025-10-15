0579fa32324dceae385cf491d57d2c0bbcbf5732
lakhan <lakhanlal3011@gmail.com>
2025-10-15 16:30:22 +0530

    gate the ws server using token

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,9 +1,23 @@
 import { WebSocketServer } from 'ws';
-
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '@repo/backend-common/config'
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', (socket) => {
+wss.on('connection', (socket, req) => {
     console.log('client connected');
+
+    const url = req.url;
+    if (!url) {
+        return;
+    };
+
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') || "";
+    const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+    if (!decoded || !decoded.userId) {
+        wss.close();
+        return;
+    }
 
     socket.on('message', (message) => {
         console.log('Message from client is: ', message.toString());
