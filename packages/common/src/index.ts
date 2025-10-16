59a525cec91b6e258e0b5090b7fd538e7600b7a6
lakhan <lakhanlal3011@gmail.com>
2025-10-16 14:36:39 +0530

    updated zod types

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const signUpSchema = z.object({
-    username: z.email(),
+    email: z.email(),
     password: z.string().min(6).max(20),
     name: z.string().min(6).max(20)
 })
