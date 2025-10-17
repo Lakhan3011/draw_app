b5afc7e5aca261143489139950acf323b9b5eaa4
lakhan <lakhanlal3011@gmail.com>
2025-10-17 23:13:56 +0530

    setup chat management

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,28 +1,98 @@
-import { WebSocketServer } from 'ws';
-import jwt from 'jsonwebtoken'
-import { JWT_SECRET } from '@repo/backend-common/config'
+import { WebSocketServer, WebSocket } from "ws";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', (socket, req) => {
-    console.log('client connected');
+interface User {
+    socket: WebSocket,
+    rooms: string[],
+    userId: string
+}
 
+const users: User[] = [];
+
+function checkUser(token: string): string | null {
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+        if (!decoded || !decoded.userId) {
+            return null;
+        }
+
+        return decoded.userId;
+    } catch (error: any) {
+        if (error.name === "TokenExpiredError") {
+            console.warn('JWT Expired: ', error.message);
+        } else {
+            console.warn('JWT Invalid: ', error.message);
+        }
+        return null;
+    }
+}
+
+
+wss.on('connection', (socket, req) => {
+    console.log('Client connected');
     const url = req.url;
     if (!url) {
-        return;
-    };
+        return null;
+    }
 
     const queryParams = new URLSearchParams(url.split('?')[1]);
-    const token = queryParams.get('token') || "";
-    const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
-    if (!decoded || !decoded.userId) {
-        wss.close();
+    const token = queryParams.get("token") || "";
+
+    const userId = checkUser(token);
+    if (userId === null) {
+        socket.close();
         return;
     }
 
-    socket.on('message', (message) => {
-        console.log('Message from client is: ', message.toString());
-        socket.send('Hi from socket server');
+    users.push({
+        userId,
+        socket,
+        rooms: []
     })
 
+    socket.on('message', (message) => {
+        const parsedData = JSON.parse(message as unknown as string);
 
+        // TODO: Does this roomID exists in db 
+        // Does this person has access to join this specific room
+        if (parsedData.type === "join_room") {
+            const user = users.find(x => x.socket === socket);
+            if (!user) {
+                return;
+            }
+            const roomId = String(parsedData.roomId);
+            if (!user.rooms.includes(roomId)) {
+                user.rooms.push(roomId);
+                console.log(`${user.userId} joins the room ${roomId}`);
+            }
+        }
+
+        if (parsedData.type === "leave_room") {
+            const user = users.find(x => x.socket === socket);
+            if (!user) return null;
+            user.rooms = user?.rooms.filter(room => room !== String(parsedData.roomId));
+            console.log(`${user.userId} left room ${parsedData.roomId}`)
+        }
+
+        // TODO: Rate limit msg not too long
+        // Auth: now anyone sends msg to any room, if one subs to room1, he mays sends msg to room2
+        if (parsedData.type === "chat") {
+            const roomId = parsedData.roomId;
+            const message = parsedData.message;
+
+            console.log(`Message to ${roomId} is ${message}`)
+
+            users.forEach(user => {
+                if (user.rooms.includes(roomId)) {
+                    user.socket.send(JSON.stringify({
+                        type: 'chat',
+                        message: message,
+                        roomId
+                    }))
+                }
+            })
+        }
+    })
 })
