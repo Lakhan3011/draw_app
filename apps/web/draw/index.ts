fa0cdb60b5027d1d42d22c895a0b865434d26859
lakhan <lakhanlal3011@gmail.com>
2025-10-24 16:56:16 +0530

    added global array to store shapes

--- apps/web/draw/index.ts
+++ apps/web/draw/index.ts
@@ -1,5 +1,20 @@
+type Shape = {
+    type: "rect";
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+} | {
+    type: "cricle";
+    centreX: number;
+    centreY: number;
+    radius: number;
+}
 
 export function initRect(canvas: HTMLCanvasElement) {
+
+    let existingShapes: Shape[] = [];
+
     const ctx = canvas.getContext('2d');
     if (!ctx) { return; }
 
@@ -19,19 +34,40 @@ export function initRect(canvas: HTMLCanvasElement) {
 
     canvas.addEventListener('mouseup', (e) => {
         clicked = false;
+        const width = e.clientX - startX;
+        const height = e.clientY - startY;
+        existingShapes.push({
+            type: "rect",
+            x: startX,
+            y: startY,
+            width,
+            height
+        })
     })
 
     canvas.addEventListener('mousemove', (e) => {
         if (clicked) {
             const width = e.clientX - startX;
             const height = e.clientY - startY;
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.fillStyle = "black";
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            clearCanvas(existingShapes, canvas, ctx);
             ctx.strokeStyle = "rgba(255,255,255)";
             ctx.strokeRect(startX, startY, width, height);
         }
-
     })
-
 }
+
+function clearCanvas(existingShapes: Shape[], canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    existingShapes.map((shape) => {
+        if (shape.type === "rect") {
+            ctx.strokeStyle = "white";
+            ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        }
+    })
+}
+
+
+// Infinite scrolling
